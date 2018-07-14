4d87478987a7a15cb3fe463927ccc08f223d58be
Michał Wieluński <michal@w-ski.dev>
2018-07-14 17:27:10 +0200

    add login script

---
+++ login.sh
@@ -0,0 +1,3 @@
+#!/bin/bash
+sleep 3
+dbus-send --type=method_call --print-reply=literal --system --dest=org.freedesktop.UPower /org/freedesktop/UPower/KbdBacklight org.freedesktop.UPower.KbdBacklight.SetBrightness int32:0
