9ce211a88d6aee12704798974e210f5a88f04aea
Michał Wieluński <michal@w-ski.dev>
2018-07-23 19:28:09 +0000

    Update install.sh

--- install.sh
+++ install.sh
@@ -37,10 +37,12 @@ apt install -y vlc
 # GOOGLE CHROME
 wget -q -O - https://dl-ssl.google.com/linux/linux_signing_key.pub | apt-key add -
 sh -c 'echo "deb [arch=amd64] http://dl.google.com/linux/chrome/deb/ stable main" >> /etc/apt/sources.list.d/google.list'
+apt update
 apt install -y google-chrome-stable
 
 # PYTHON
 add-apt-repository ppa:deadsnakes/ppa
+apt update
 apt install -y python3.5
 apt install -y python3.6
 apt install -y python3.7
