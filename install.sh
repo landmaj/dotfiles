4807b7c461bdcc501c3072dbc59d14babbbeea93
Michał Wieluński <michal@w-ski.dev>
2018-07-24 18:30:13 +0000

    Update install.sh

--- install.sh
+++ install.sh
@@ -62,6 +62,8 @@ addgroup --system docker
 adduser $USER docker
 newgrp docker
 snap install docker --classic
+docker volume create portainer_data
+docker run -d -p 9000:9000 -v /var/run/docker.sock:/var/run/docker.sock -v portainer_data:/data portainer/portainer
 
 
 # AUTOSTART
