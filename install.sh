1531d842cec485554ae9f87f0e9c7b650ff99150
Michał Wieluński <michal@w-ski.dev>
2018-10-21 15:09:45 +0200

    remove Portainer (Docker GUI)

--- install.sh
+++ install.sh
@@ -99,8 +99,6 @@ ${USER_HOME}/.local/bin/pipsi install gitup
 addgroup --system docker
 adduser ${SUDO_USER} docker
 snap install docker --classic
-docker volume create portainer_data
-docker run -d -p 9000:9000 -v /var/run/docker.sock:/var/run/docker.sock -v portainer_data:/data portainer/portainer
 
 # THEFUCK
 apt install -y thefuck
