f6a645447538250f0fed3e9e339cdd904d75ccf4
Michał Wieluński <michal@w-ski.dev>
2018-10-21 15:09:00 +0200

    remove newgrp command causing script to fail

--- install.sh
+++ install.sh
@@ -98,7 +98,6 @@ ${USER_HOME}/.local/bin/pipsi install gitup
 # DOCKER
 addgroup --system docker
 adduser ${SUDO_USER} docker
-newgrp docker
 snap install docker --classic
 docker volume create portainer_data
 docker run -d -p 9000:9000 -v /var/run/docker.sock:/var/run/docker.sock -v portainer_data:/data portainer/portainer
