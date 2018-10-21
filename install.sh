0a258865928cff22858248e6a9377b9a0d770c84
Michał Wieluński <michal@w-ski.dev>
2018-10-21 15:22:49 +0200

    minimize sudo usage

--- install.sh
+++ install.sh
@@ -1,24 +1,21 @@
 #!/usr/bin/env bash
 
-if [ "$EUID" -ne 0 ]
-  then echo "Please run as root"
-  exit
-fi
+# SUDO WITHOUT PASSWORD
+sudo echo "\n ${USER} ALL=(ALL) NOPASSWD:ALL" >> /etc/sudoers
 
 # UNWANTED PACKAGES
-apt remove -y \
+sudo apt remove -y \
     plank \
     atril \
     onboard \
     onboard-common \
     orca \
     xzoom
-
-rm -rf /usr/share/applications/screensavers
-apt autoremove -y
+sudo apt autoremove -y
+sudo rm -rf /usr/share/applications/screensavers
 
 # BASIC i3
-apt install -y i3-wm rofi
+sudo apt install -y i3-wm rofi
 
 # GSETTINGS
 gsettings set org.mate.mate-menu hot-key ''
@@ -30,31 +27,26 @@ gsettings set org.mate.power-manager backlight-battery-reduce false
 gsettings set org.mate.session required-components-list "['windowmanager', 'panel']"
 gsettings set org.mate.session.required-components windowmanager 'i3'
 
-# USER HOME DIRECTORY
-USER_HOME=$(getent passwd ${SUDO_USER} | cut -d: -f6)
-
 # AUTOSTART
-cp -r ./autostart ${USER_HOME}/.config/
+cp -r ./autostart ${HOME}/.config/
 
 # CONFIG FILES
-cp -r ./config ${USER_HOME}/
+cp -r ./config ${HOME}/
 
 # SCRIPTS
-cp -r ./bin ${USER_HOME}/
+cp -r ./bin ${HOME}/
 
 # FONTS
-cp -r .fonts ${USER_HOME}/
+cp -r .fonts ${HOME}/
 
 # ALIASES
-echo "\n" >> ${USER_HOME}/.bashrc
-echo "alias vifm=\"source ${USER_HOME}/bin/vf"
-echo "alias venv=\"source ${USER_HOME}/bin/venv.sh\"" >> ${USER_HOME}/.bashrc
+echo "\n" >> ${HOME}/.bashrc
+echo "alias vifm=\"source ~/bin/vf"
+echo "alias venv=\"source ~/bin/venv.sh\"" >> ${HOME}/.bashrc
 
-# SUDO WITHOUT PASSWORD
-echo "${SUDO_USER} ALL=(ALL) NOPASSWD:ALL" >> /etc/sudoers
 
 # BASIC APPLICATIONS
-apt install -y \
+sudo apt install -y \
     ntp \
     dconf-editor \
     arandr \
@@ -79,46 +71,46 @@ apt install -y \
 
 # SUBLIME
 wget -qO - https://download.sublimetext.com/sublimehq-pub.gpg | sudo apt-key add -
-apt-get install apt-transport-https
+sudo apt install apt-transport-https
 echo "deb https://download.sublimetext.com/ apt/stable/" | sudo tee /etc/apt/sources.list.d/sublime-text.list
-apt-get update
-apt-get install sublime-merge sublime-text
+sudo apt update
+sudo apt install -y sublime-merge sublime-text
 
 # PIPSI
-apt install -y python3-venv
+sudo apt install -y python3-venv
 cd /tmp
 curl https://raw.githubusercontent.com/mitsuhiko/pipsi/master/get-pipsi.py | python3 - --src=git+https://github.com/mitsuhiko/pipsi.git\#egg=pipsi
-echo 'export PATH="$USER_HOME/.local/bin:$PATH"' >> ${USER_HOME}/.bashrc
+echo 'export PATH="${HOME}/.local/bin:$PATH"' >> ${HOME}/.bashrc
 
-${USER_HOME}/.local/bin/pipsi install flake8
-${USER_HOME}/.local/bin/pipsi install ansible
-${USER_HOME}/.local/bin/pipsi install pi3-switch
-${USER_HOME}/.local/bin/pipsi install gitup
+${HOME}/.local/bin/pipsi install flake8
+${HOME}/.local/bin/pipsi install ansible
+${HOME}/.local/bin/pipsi install pi3-switch
+${HOME}/.local/bin/pipsi install gitup
 
 # THEFUCK
-apt install -y thefuck
-echo "eval \$(thefuck --alias)" >> ${USER_HOME}/.bashrc
+sudo apt install -y thefuck
+echo "eval \$(thefuck --alias)" >> ${HOME}/.bashrc
 
 # VIM
-apt install -y vim-gtk
-git clone --depth=1 https://github.com/amix/vimrc.git ${USER_HOME}/.vim_runtime
-bash  ${USER_HOME}/.vim_runtime/install_awesome_vimrc.sh
+sudo apt install -y vim-gtk
+git clone --depth=1 https://github.com/amix/vimrc.git ${HOME}/.vim_runtime
+bash  ${HOME}/.vim_runtime/install_awesome_vimrc.sh
 echo 'augroup numbertoggle
   autocmd!
   autocmd BufEnter,FocusGained,InsertLeave * set relativenumber
   autocmd BufLeave,FocusLost,InsertEnter   * set norelativenumber
-augroup END' >> ${USER_HOME}/.vimrc
+augroup END' >> ${HOME}/.vimrc
 
 # DOCKER
-addgroup --system docker
-adduser ${SUDO_USER} docker
+sudo addgroup --system docker
+sudo adduser ${USER} docker
 
 # SNAPS
-nohup snap install spotify &
-nohup snap install pycharm-professional --classic &
-nohup snap install goland --classic &
-nohup snap install insomnia &
-nohup snap install docker --classic &
+nohup sudo snap install spotify &
+nohup sudo snap install pycharm-professional --classic &
+nohup sudo snap install goland --classic &
+nohup sudo snap install insomnia &
+nohup sudo snap install docker --classic &
 
-apt update
-apt upgrade -y
+sudo apt update
+sudo apt upgrade -y
