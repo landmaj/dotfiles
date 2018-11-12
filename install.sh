bb55653c28096d74d53a45972bd5c6274c50cb20
Michał Wieluński <michal@w-ski.dev>
2018-11-12 21:17:12 +0100

    add htop

--- install.sh
+++ install.sh
@@ -78,8 +78,8 @@ sudo apt install -y \
     xclip \
     pinta \
     exuberant-ctags \
-    fonts-powerline
-
+    fonts-powerline \
+    htop
 
 # SUBLIME
 wget -qO - https://download.sublimetext.com/sublimehq-pub.gpg | sudo apt-key add -
