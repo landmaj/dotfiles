84c31ec5f6b9ba4e8d0c0deec8688f217d27b409
Michał Wieluński <michal@w-ski.dev>
2019-09-26 18:22:43 +0200

    add fd

--- install.sh
+++ install.sh
@@ -88,6 +88,7 @@ wget -O /tmp/light.deb https://github.com/haikarainen/light/releases/download/v1
 wget -O /tmp/playerctl.deb https://github.com/altdesktop/playerctl/releases/download/v2.0.2/playerctl-2.0.2_amd64.deb && sudo dpkg -i /tmp/playerctl.deb
 wget -O /tmp/alacritty.deb https://github.com/jwilm/alacritty/releases/download/v0.3.3/Alacritty-v0.3.3-ubuntu_18_04_amd64.deb && sudo dpkg -i /tmp/alacritty.deb
 wget -O /tmp/bat.deb https://github.com/sharkdp/bat/releases/download/v0.12.1/bat_0.12.1_amd64.deb && sudo dpkg -i /tmp/bat.deb
+wget -O /tmp/fd.deb https://github.com/sharkdp/fd/releases/download/v7.4.0/fd_7.4.0_amd64.deb && sudo dpkg -i /tmp/fd/deb
 wget -O /tmp/exa.zip https://github.com/ogham/exa/releases/download/v0.9.0/exa-linux-x86_64-0.9.0.zip && unzip /tmp/exa.zip && sudo mv exa-linux-x86_64 /usr/local/bin/exa
 
 # bash aliases
