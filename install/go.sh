347cdef67af6463595c272c76a055369cc20be65
Michał Wieluński <michal@w-ski.dev>
2020-08-18 20:06:59 +0200

    add gvm

--- install/go.sh
+++ install/go.sh
@@ -3,3 +3,8 @@
 sudo apt-get install -y golang-go
 
 sudo apt install goland
+
+# go version manager (gvm)
+sudo apt install bison
+bash < <(curl -s -S -L https://raw.githubusercontent.com/moovweb/gvm/master/binscripts/gvm-installer)
+
