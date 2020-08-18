931505887b04bf25a8251e352dd8e87a014c725a
Michał Wieluński <michal@w-ski.dev>
2020-08-18 19:57:15 +0200

    hold bat version

--- install/rust_tools.sh
+++ install/rust_tools.sh
@@ -2,6 +2,7 @@
 
 # bat (cat replacement)
 wget -O /tmp/bat.deb https://github.com/sharkdp/bat/releases/download/v0.12.1/bat_0.12.1_amd64.deb && sudo dpkg -i /tmp/bat.deb
+sudo apt-mark hold bat  # on Pop!_OS apt replaces bat with some other app
 
 # fd (find replacement)
 wget -O /tmp/fd.deb https://github.com/sharkdp/fd/releases/download/v7.4.0/fd_7.4.0_amd64.deb && sudo dpkg -i /tmp/fd.deb
