41fe4a197e58044ccb05e35f7465afd485810bb5
Michał Wieluński <michal@w-ski.dev>
2020-10-24 13:55:44 +0200

    fix ansible install command

--- install/pipx.sh
+++ install/pipx.sh
@@ -7,10 +7,10 @@ python3 -m pipx ensurepath
 "${HOME}"/.local/bin/pipx install tldr
 "${HOME}"/.local/bin/pipx install python-language-server
 
-"${HOME}"/.local/bin/pipx install ansible
+"${HOME}"/.local/bin/pipx install ansible --include-deps
 "${HOME}"/.local/bin/pipx install docker-compose
 
 
-"${HOME}"/.local/bin/pipx install gitup
+${HOME}"/.local/bin/pipx install gitup
 echo "alias up=\"gitup --prune ~/github/\"" >> "${HOME}"/.bashrc
 
