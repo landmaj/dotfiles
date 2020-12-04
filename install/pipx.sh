ba092374465f992feb04bac2f32d763cf6a35a37
Michał Wieluński <michal@w-ski.dev>
2020-12-04 11:05:24 +0100

    add gitup

--- install/pipx.sh
+++ install/pipx.sh
@@ -6,7 +6,7 @@ python3 -m pipx ensurepath
 "${HOME}"/.local/bin/pipx install pre-commit
 "${HOME}"/.local/bin/pipx install tldr
 "${HOME}"/.local/bin/pipx install python-language-server
-
+"${HOME}"/.local/bin/pipx install gitup
 "${HOME}"/.local/bin/pipx install ansible --include-deps
 "${HOME}"/.local/bin/pipx install docker-compose
 
