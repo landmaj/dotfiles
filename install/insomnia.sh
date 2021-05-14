93d9118e52d87231ddfd844eda2bf0e5514c18fc
Michał Wieluński <michal@w-ski.dev>
2021-05-14 11:00:53 +0200

    update insomnia source

--- install/insomnia.sh
+++ install/insomnia.sh
@@ -1,10 +1,8 @@
 #!/bin/bash -e
 
-# Add the release PGP keys:
-curl -s https://insomnia.rest/keys/debian-public.key.asc | sudo apt-key add -
-
 # Add the channel to your APT sources:
-echo "deb https://dl.bintray.com/getinsomnia/Insomnia /" | sudo tee /etc/apt/sources.list.d/insomnia.list
+echo "deb [trusted=yes arch=amd64] https://download.konghq.com/insomnia-ubuntu/ default all" \
+    | sudo tee -a /etc/apt/sources.list.d/insomnia.list
 
 # Update and install:
 sudo apt update && sudo apt install -y insomnia
