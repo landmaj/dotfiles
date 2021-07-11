f9f97be324a2dda49457b72b36a24c76f2af0081
Michał Wieluński <michal@w-ski.dev>
2021-07-11 22:43:37 +0200

    fix direnv

--- mac_os/setup.sh
+++ mac_os/setup.sh
@@ -7,6 +7,9 @@ echo "alias venv=\"source ~/bin/venv\"" >> "${HOME}"/.zshrc
 echo "alias ee=\"exa -lagH\"" >> "${HOME}"/.zshrc
 echo "" >> "${HOME}"/.zshrc
 
+echo "eval \"$(direnv hook zsh)\"" >> "${HOME}/.zshrc
+echo "" >> "${HOME}"/.zshrc
+
 cp "${cwd}"/miscellaneous/.gitignore "${HOME}"/
 git config --global core.excludesfile ~/.gitignore
 
