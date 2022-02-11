c26a73d4b54d62eee5197f4cb24b700f8758d856
Michał Wieluński <michal@w-ski.dev>
2022-02-11 18:43:14 +0100

    refactor macOS

---
+++ macOS/brew.sh
@@ -0,0 +1,58 @@
+#!/bin/zsh -e
+
+# /bin/bash -c "$(curl -fsSL https://raw.githubusercontent.com/Homebrew/install/HEAD/install.sh)"
+# echo 'eval "$(/opt/homebrew/bin/brew shellenv)"' >>"${HOME}"/.zprofile
+# eval "$(/opt/homebrew/bin/brew shellenv)"
+
+brew install \
+  autojump \
+  bat \
+  coreutils \
+  direnv \
+  exercism \
+  git-crypt \
+  gitup \
+  graphviz \
+  pipx \
+  pre-commit \
+  pure \
+  tldr \
+  vdirsyncer \
+  wget \
+  zsh-completions \
+  ;
+
+brew install syncthing && brew services start syncthing
+
+brew install \
+  elixir \
+  go \
+  python@3 \
+  ;
+
+brew install --cask \
+  homebrew/cask-drivers/steelseries-exactmouse-tool \
+  ;
+
+brew install --cask \
+  autodesk-fusion360 \
+  elmedia-player \
+  firefox \
+  insomnia \
+  jettison \
+  karabiner-elements \
+  keepassxc \
+  messenger \
+  monitorcontrol \
+  obsidian \
+  postgres-unofficial \
+  prusaslicer \
+  qnapi \
+  raspberry-pi-imager \
+  spotify \
+  switchresx \
+  temurin \
+  transmission \
+  utm \
+  visual-studio-code \
+  ;
