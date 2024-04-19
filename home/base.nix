5f0de777bfc9e699f7bb426098ff9eba2ac12f1c
Michał Wieluński <michal.wielunski@audiotekagroup.com>
2024-04-19 09:05:08 +0200

    update git aliases

--- home/base.nix
+++ home/base.nix
@@ -40,8 +40,9 @@
         gs = "git status";
         ga = "git add *";
         gm = "git commit -m";
-        gp = "git push";
-        gg = "git log --graph --abbrev-commit --decorate --date=relative --format=format:'%C(bold blue)%h%C(reset) - %C(bold green)(%ar)%C(reset) %C(white)%s%C(reset) %C(dim white)- %an%C(reset)%C(bold yellow)%d%C(reset)' --all";
+        log = "git log --graph --abbrev-commit --decorate --date=relative --format=format:'%C(bold blue)%h%C(reset) - %C(bold green)(%ar)%C(reset) %C(white)%s%C(reset) %C(dim white)- %an%C(reset)%C(bold yellow)%d%C(reset)' --all";
+        pull = "git pull";
+        push = "git push";
 
         j = "z";
 
