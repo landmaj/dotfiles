d0e9f2b016c2c7691d9443dcab451071d917593a
Michał Wieluński <michal@wielunski.net>
2024-04-24 14:22:43 +0200

    update templates

--- templates/blank/flake.nix
+++ templates/blank/flake.nix
@@ -1,20 +1,27 @@
 {
   inputs = {
     nixpkgs.url = "github:nixos/nixpkgs";
-    utils.url = "github:numtide/flake-utils";
   };
 
-  outputs = { self, nixpkgs, utils }:
-    utils.lib.eachDefaultSystem (system:
-      let
-        pkgs = import nixpkgs { inherit system; };
-      in
-      {
-        devShells.default = with pkgs; mkShell {
+  outputs =
+    { self, nixpkgs }:
+    let
+      forAllSystems = function:
+        nixpkgs.lib.genAttrs [
+          "x86_64-linux"
+          "x86_64-darwin"
+          "aarch64-linux"
+          "aarch64-darwin"
+        ]
+          (system: function nixpkgs.legacyPackages.${system});
+    in
+    {
+      devShells = forAllSystems (pkgs: {
+        default = pkgs.mkShell {
           packages = with pkgs; [
-            # SYSTEM PACKAGES
+            # PACKAGES
           ];
         };
-      }
-    );
+      });
+    };
 }
