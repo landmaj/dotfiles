0359cd051db0d0850f58b7ee710594b0cab5e1a5
Michał Wieluński <michal@wielunski.net>
2025-10-10 19:12:21 +0200

    add Automator scripts

--- home/home.nix
+++ home/home.nix
@@ -16,6 +16,8 @@
     file = {
       ".gitconfig".source = ../config/home/.gitconfig;
       ".ssh/key.pub".source = ../config/home/key.pub;
+      "Library/Services/Display [native].workflow".source = ../config/Automator/display_native.workflow;
+      "Library/Services/Display [scaled].workflow".source = ../config/Automator/display_scaled.workflow;
     };
   };
 }
