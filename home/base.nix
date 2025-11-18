7f7408ec25a9f73f8f8c38364d40c838231d57a8
Michał Wieluński <michal@wielunski.net>
2025-11-18 08:49:38 +0100

    modify log alias

--- home/base.nix
+++ home/base.nix
@@ -56,7 +56,7 @@
         branch = "git branch";
         checkout = "git checkout";
         commit = "git commit";
-        log = "git log --graph --abbrev-commit --decorate --date=relative --format=format:'%C(bold blue)%h%C(reset) - %C(bold green)(%ar)%C(reset) %C(white)%s%C(reset) %C(dim white)- %an%C(reset)%C(bold yellow)%d%C(reset)' --all";
+        log = "git log --abbrev-commit --decorate --date=relative --format=format:'%C(bold blue)%h%C(reset) - %C(bold green)(%ar)%C(reset) %C(white)%s%C(reset) %C(dim white)- %an%C(reset)%C(bold yellow)%d%C(reset)'";
         pull = "git pull";
         push = "git push";
         status = "git status";
